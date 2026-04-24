5ecb0ccc4c1166b97952cb5c2cc2bef092b532da
Master Robert <officialelmalik@gmail.com>
2026-04-24 03:01:24 +0000

    MASTER SEAL: v3.10 Unrestricted | Physical Vault Backed | Matrix Unified

--- js/config.js
+++ js/config.js
@@ -2,5 +2,6 @@ export const CONFIG = {
     API_URL: "https://neosai-engine-4bgnpfbrs9yn.buildaispace.app",
     MASTER_KEY: "1970645832101",
     PROJECT_ID: "neosai-apex-v3",
-    VERSION: "3.5 UNRESTRICTED"
+    VERSION: "3.10 PHYSICALIZED",
+    BENEFITS: "SDVOSB_UNRESTRICTED"
 };
